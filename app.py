18568b38c712f001aaf9015a5659beb9f488d7dd
tiagopatzz <tiago.patzlaff@universo.univates.br>
2026-06-16 18:12:22 -0300

    ajustes bugs

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'tiago.patzlaff@universo.univates.br'
-app.config['MAIL_PASSWORD'] = 'senha'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'senha')
 mail = Mail(app)
 
 # Permite servir atras do NGINX em /homolog ou /prod (via APP_PREFIX).
